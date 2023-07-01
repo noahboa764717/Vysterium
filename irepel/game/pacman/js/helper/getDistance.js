3fe66e272d35517d92f0cd595342a686e3c75af7
0xkkl <kaydan@leozc.com>
2023-06-30 18:10:19 -0700

    changed uv bundles

--- irepel/game/pacman/js/helper/getDistance.js
+++
@@ -1,5 +0,0 @@
-// Distance between two tiles.
-export default (tileA, tileB) => {
-    const x = tileA.x, x1 = tileB.x, y = tileA.y, y1 = tileB.y;
-    return Math.sqrt(Math.pow(x - x1, 2) + Math.pow(y - y1, 2));
-}
--- irepel/game/pacman/js/helper/rnd.js
+++
@@ -1,2 +0,0 @@
-// Return random number between 0 and total less one.
-export default total => Math.floor((Math.random() * total));

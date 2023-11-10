aa23a214ff91ef87270ae44823d00859529e96f3
urasdbwd <extratoppingsplease123@gmail.com>
2023-11-09 16:12:46 -0800

    idk bruh

--- sw.js
+++ sw.js
@@ -17,25 +17,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     bareReady.then(() => {
       return sw.fetch(event).then(response => {
-        // Check if the request is for an HTML document
-        if (event.request.headers.get('Accept').includes('text/html')) {
-          // Make sure we only modify the responses that are OK and HTML
-          if (!response.ok || !response.headers.get('Content-Type') || response.headers.get('Content-Type').indexOf('text/html') === -1) {
-            return response;
-          }
-          return response.clone().text().then(body => {
-            // Modify the HTML body
-            let newBody = body.replace('</body>', `
-            <style>${styles}</style><script>${guimenua}</script></body>`);
-            
-            // Create a new response
-            return new Response(newBody, {
-              headers: response.headers
-            });
-          });
-        } else {
           return response;
-        }
       });
     })
   );

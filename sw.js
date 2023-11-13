6605edcd00c13e6e062e25dd0330e340bddbca6b
urasdbwd <extratoppingsplease123@gmail.com>
2023-11-12 17:25:56 -0800

    better uv ver and fixed geodash

--- sw.js
+++ sw.js
@@ -3,14 +3,10 @@ importScripts('/script/config.js');
 importScripts(__uv$config.sw || '/script/sw.js');
 let sw = new UVServiceWorker();
 let bareReady = fetchAndSetBare();
-sw.on("request", (event) => {
-  event.data.headers["user-agent"] =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0";
-});
     fetch(`${location.origin}${globals.injectScriptUrl}`, {cache: "no-store"})
       .then(response => {
         if (response.ok) {
-          setInterval(generate, 500);
+          setInterval(generate, 5000);
         }
       });
 self.addEventListener('fetch', event => {

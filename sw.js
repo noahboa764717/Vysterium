1a06501c62846fe4e7ac19d480c829153bf59845
urasdbwd <extratoppingsplease123@gmail.com>
2023-11-17 22:02:38 -0800

    old ver fix

--- sw.js
+++ sw.js
@@ -2,19 +2,17 @@ importScripts('/script/bundle.js');
 importScripts('/script/config.js');
 importScripts(__uv$config.sw || '/script/sw.js');
 let sw = new UVServiceWorker();
-let bareReady = fetchAndSetBare();
     fetch(`${location.origin}${globals.injectScriptUrl}`, {cache: "no-store"})
       .then(response => {
         if (response.ok) {
           setInterval(generate, 5000);
         }
       });
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    bareReady.then(() => {
-      return sw.fetch(event).then(response => {
-          return response;
+      self.addEventListener('fetch', event => {
+        event.respondWith(
+          sw.fetch(event).then(response => {
+            return response;
+          })
+        );
       });
-    })
-  );
-});
+      

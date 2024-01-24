9007271f1961fbe01139b1dabf72c6d251eeb7a0
amongus <102772348+urasdbwd@users.noreply.github.com>
2024-01-24 14:51:34 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,10 @@ let sw = new UVServiceWorker();
           setInterval(generate, 5000);
         }
       });
+sw.on("request", (event) => {
+  event.data.headers["user-agent"] =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0";
+});
       self.addEventListener('fetch', event => {
         event.respondWith(
           sw.fetch(event).then(response => {
